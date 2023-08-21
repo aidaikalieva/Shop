171a866604110c3e434bcb99745f7db7c2041519
Aidai <aidai@MacBook-Air-Aidai.local>
2023-08-21 18:51:19 +0600

    first push

---
+++ CosmeticsWebsite.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+function CosmeticsWebsite(){
+    return(
+        <div>
+            <header>
+                <h1>MOON</h1>
+                <nav>
+                    <ul>
+                        <li>Главная</li>
+                        <li>Продукция</li>
+                        <li>О нас</li>
+                        <li>Контакты</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
+}
